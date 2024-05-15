31d96468158ae8aca3c65222134245a2b712a723
Stckrz <rin449@gmail.com>
2024-05-15 13:16:03 -0500

    grr

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const predictionsRouter = require('./routes/predictions');
 const commentsRouter = require('./routes/comments')
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
